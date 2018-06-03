00979d863a89682189d80ed1532a1bd882d8cb06
supak <supak@afs.org>
2018-06-03 18:18:06 +0700

    add component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ModelDrivenFormComponent } from './model-driven-form/model-driven-form.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+
+const routes: Routes = [
+  {path: 'model', component: ModelDrivenFormComponent},
+  {path: 'template', component: TemplateDrivenFormComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})    
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModelDrivenFormComponent } from './model-driven-form/model-driven-form.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelDrivenFormComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
