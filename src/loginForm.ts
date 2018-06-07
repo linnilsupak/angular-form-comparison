6c4ff072aa7af786a6e6979861cea1dc67f6f8b6
supak <supak@afs.org>
2018-06-07 20:21:51 +0700

    add constructor

--- src/loginForm.ts
+++ src/loginForm.ts
@@ -1,4 +1,9 @@
 export default class loginForm{
     userName: string;
     password: string;
+
+    constructor(obj?:any){
+      this.userName = obj.userName || '';
+      this.password = obj.password || '';
+    }
   }
