a4cc42442d944788f3bcd9e26e81f4f7d3de7063
supak <supak@afs.org>
2018-06-24 11:46:10 +0700

    add refer element

--- src/app/template-driven-form/template-driven-form.component.spec.ts
+++ src/app/template-driven-form/template-driven-form.component.spec.ts
@@ -4,26 +4,38 @@ import { TemplateDrivenFormComponent } from './template-driven-form.component';
 import { MaterialModule } from '../materia.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 
 describe('TemplateDrivenFormComponent', () => {
   let component: TemplateDrivenFormComponent;
   let fixture: ComponentFixture<TemplateDrivenFormComponent>;
+  let submitEl: DebugElement;
+  let userNameEl: DebugElement;
+  let passwordEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [MaterialModule, FormsModule, BrowserAnimationsModule],
       declarations: [ TemplateDrivenFormComponent ]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TemplateDrivenFormComponent);
     component = fixture.componentInstance;
+    submitEl = fixture.debugElement.query(By.css('button'))
+    userNameEl = fixture.debugElement.query(By.css('input[type=text]'));
+    passwordEl = fixture.debugElement.query(By.css('input[type=password]'));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('form invalid when empty', () => {
+  //   expect(component.loginForm.valid).toBeFalsy;
+  // });
 });
