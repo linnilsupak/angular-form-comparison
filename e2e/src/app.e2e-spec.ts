e3105ad1543810620ecd6026e1bde0be5855f787
supak <supak@afs.org>
2018-06-25 23:36:03 +0700

    add e2e test spec

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,18 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should redirect to /model', () =>{
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-form-comparation!');
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/model');
+  });
+
+  it('click template driven form button, should redirect to /template', () =>{
+    page.getTemplateDrivenButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/template');
+  });
+
+  it('click model driven form button, should redirect to /model', () => {
+    page.getModelDrivenButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/model');
   });
 });
